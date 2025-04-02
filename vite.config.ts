52aaf8272da745c516bf68a343bdd28e7ba4d371
ghodnyuk1 <deciles.politer_0h@icloud.com>
2025-04-02 16:09:38 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './', // 👈 ДОДАЙ ОЦЕ!
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
